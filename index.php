160c16351a9cbf9bdec9abc40afaeeb5618eab54
RaulCasar <104854402+RaulCasar@users.noreply.github.com>
2024-06-02 13:31:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <div class="center-align">
 	<ul>
 		<div class="left-align">
-			<li><a href="read.php"><strong>Ver catalago</strong></a></li>
-			<li><a href="insert.php"><strong>Agregar producto</strong></a></li>
+			<li><a href="read.php"><strong>Ver todos los productos</strong></a></li>
+			<li><a href="insert.php"><strong>ingresar producto</strong></a></li>
 			<li><a href="update.php"><strong>Actualizar producto</strong></a></li>
 			<li><a href="delete.php"><strong>Remover producto</strong></a></li>
 		</div>
