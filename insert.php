3ed052b25976767d8e843a62adb97212b0c2427b
RaulCasar <104854402+RaulCasar@users.noreply.github.com>
2024-06-03 14:17:14 -0600

    Update insert.php

--- insert.php
+++ insert.php
@@ -13,7 +13,7 @@
 
         // Establecer la conexión
         $conn = mysqli_init();
-        mysqli_ssl_set($conn, NULL, NULL, $sslcert, NULL, NULL);
+        mysqli_ssl_set($conn, NULL, NULL, NULL, NULL);
         if (!mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL)) {
             die('Failed to connect to MySQL: ' . mysqli_connect_error());
         }
