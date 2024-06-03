ad9f81de4a2086b99b2f6d9c7022176b1556539c
RaulCasar <104854402+RaulCasar@users.noreply.github.com>
2024-06-03 17:37:04 -0600

    Create login.php

---
+++ login.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+
+// Definir usuario y contraseña
+$stored_username = 'admin';
+$stored_password = 'password';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Verificar usuario y contraseña
+    if ($username === $stored_username && $password === $stored_password) {
+        $_SESSION['loggedin'] = true;
+        header('Location: index.php');
+        exit;
+    } else {
+        $error = "Usuario o contraseña incorrectos";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+</head>
+<body>
+    <div class="login-container">
+        <h2>Login</h2>
+        <form action="login.php" method="post">
+            <?php if (!empty($error)) : ?>
+                <p class="error"><?php echo $error; ?></p>
+            <?php endif; ?>
+            <label for="username">Usuario:</label>
+            <input type="text" id="username" name="username" required>
+            <label for="password">Contraseña:</label>
+            <input type="password" id="password" name="password" required>
+            <button type="submit">Ingresar</button>
+        </form>
+    </div>
+</body>
+</html>
