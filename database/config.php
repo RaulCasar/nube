72215bb9f605de6cfc1132a345aba44065ee715a
RaulCasar <104854402+RaulCasar@users.noreply.github.com>
2024-06-03 13:53:29 -0600

    Update config.php

--- database/config.php
+++ database/config.php
@@ -1,9 +1,14 @@
 <?php
-
-// Configuration for database connection
-
 $host       = getenv('DB_HOST');
 $username   = getenv('DB_USERNAME');
 $password   = getenv('DB_PASSWORD');
 $db_name     = getenv('DB_DATABASE');
-$sslcert    = "ssl/DigiCertGlobalRootCA.crt.pem";
+
+// Crear la conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
